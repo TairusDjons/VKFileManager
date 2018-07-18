61343715c3cbcdca28b009a5984171eac5193a37
Victor Alekseev <victor@macbook-pro-victor.local>
2018-07-18 08:42:39 +0300

    Initial Commit

---
+++ VKFileManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKFileManager
+//
+//  Created by Victor Alekseev on 18/07/2018.
+//  Copyright © 2018 Victor Alekseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
